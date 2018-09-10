1e70f09694b9d19282a2aa34d02d0be08553e056
Christopher <39324289+colasd@users.noreply.github.com>
2018-09-10 13:45:41 +0900

    아 왜....

--- src/main/java/com/sesoc/ictmd/Interface/TrendDAO.java
+++ src/main/java/com/sesoc/ictmd/Interface/TrendDAO.java
@@ -1,7 +1,11 @@
 package com.sesoc.ictmd.Interface;
 
+import java.util.List;
+
+import com.sesoc.ictmd.vo.BasicTrendData;
+
 public interface TrendDAO {
 	
-	
+	public List<BasicTrendData> BasicModelChart();
 	
 }
--- src/main/java/com/sesoc/ictmd/vo/BasicTrendData.java
+++ src/main/java/com/sesoc/ictmd/vo/BasicTrendData.java
@@ -4,14 +4,16 @@ public class BasicTrendData {
 
 	private String dataName;
 	private String dataNum;
+	private String dataPercentage;
 
 	public BasicTrendData() {
+		
 	}
 
-	public BasicTrendData(String dataName, String dataNum) {
-		super();
+	public BasicTrendData(String dataName, String dataNum, String dataPercentage) {
 		this.dataName = dataName;
 		this.dataNum = dataNum;
+		this.dataPercentage = dataPercentage;
 	}
 
 	public String getDataName() {
@@ -30,9 +32,18 @@ public class BasicTrendData {
 		this.dataNum = dataNum;
 	}
 
+	public String getDataPercentage() {
+		return dataPercentage;
+	}
+
+	public void setDataPercentage(String dataPercentage) {
+		this.dataPercentage = dataPercentage;
+	}
+
 	@Override
 	public String toString() {
-		return "BasicTrendData [dataName=" + dataName + ", dataNum=" + dataNum + "]";
+		return "BasicTrendData [dataName=" + dataName + ", dataNum=" + dataNum + ", dataPercentage=" + dataPercentage
+				+ "]";
 	}
 
 }
